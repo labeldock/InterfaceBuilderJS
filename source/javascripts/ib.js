02b9b2f1838c230abc33ab05ee873505a15e52e2
HO-JUNG AHN <labeldock@me.com>
2016-10-10 23:39:01 +0900

    update
    - Interface builder extend method
    - Query select engine bindings

---
+++ source/javascripts/ib.js
@@ -0,0 +1,177 @@
+(function(window){
+	
+	var IB = window.ib = function(select){ return (new InterfaceDefine(select)); };
+	IB.version = "0.0.1";
+	
+	var CFBREAKER = {$:"this is enum breaker"};
+	
+	var CFIS = {
+		VENDER:function(o){ return (typeof o === "object" && o !== null ) ? ("jquery" in o) ? true : false : false; },
+		DATA:function(a){ return (typeof a === "object" && a !== null ) ? (((a instanceof Array || a instanceof NodeList || a instanceof HTMLCollection || CFIS.VENDER(a) || ( !isNaN(a.length) && isNaN(a.nodeType))) && !(a instanceof Window) ) ? true : false) : false; }.bind(),
+		ARRAY:function(a){ return a instanceof Array; }.bind(),
+		ELEMENT:function(a){ if(a == null) return false; if(typeof a === "object") if(a.nodeType == 1) return true; return false; },
+		NODE:function(a){ if(a == null) return false; if(typeof a === "object") if(typeof a.nodeType == "number") return true; return false; },
+		USELESS:function(o){ 
+			if (typeof o === "undefined")return true;
+			if (typeof o === "string")return o.trim().length < 1 ? true : false;
+			if (typeof o === "object"){
+				if(o instanceof RegExp) return false;
+				if(CFIS.ELEMENT(o)) return false;
+				if(o == null ) return true;
+				if(CFIS.DATA(o)) {
+					o = o.length;
+				} else {
+					var count = 0; for (var prop in o) { count = 1; break; } o = count;
+				}
+			}
+			if (typeof o === "number")return o < 1 ? true : false;
+			if (typeof o === "function")return false;
+			if (typeof o === "boolean")return !o;
+			console.warn("CFIS.USELESS::unknown type",o);
+			return true;
+		},
+		USEABLE:function(o){
+			return !CFIS.USELESS(o);
+		}
+	};
+	
+	var CFAS = {
+		MIRROR:function(){
+			if(CFIS.ARRAY(v)) return new Array(v);
+			return v;
+		},
+		DATA:function(v){
+			if(CFIS.ARRAY(v)) return v;
+			
+			if( CFIS.DATA(v) ) { 
+				if("toArray" in v){ 
+					return Array.prototype.slice.apply(v.toArray()); 
+				} else {
+					var mvArray = [];
+					for(var i=0,l=v.length;i<l;i++) mvArray.push(v[i]); 
+					return mvArray;
+				} 
+			}
+			
+			if(v||v==0) return [v];
+			
+			return [];
+		}.bind(),
+		PROPS:function(k,v){
+			if(typeof k === "object") return k;
+			if(typeof k === "string" || typeof k === "number") return {k:v};
+			return {};
+		}
+	};
+	
+	//minor version method
+	var TODO = {};
+	
+	var IBProcesser = function(process){
+		return function(){
+			var newque = Array.prototype.slice.call(this.$que);
+			var ibargs = Array.prototype.slice.call(arguments);
+			
+			newque.push(function(callback){
+				var select = this.$select;
+				var args = Array.prototype.slice.call(ibargs);
+				args.unshift(callback);
+				
+				return process.apply(select,args);
+			});
+			
+			return new InterfaceDefine(this.$select,newque);
+		};
+	};
+	
+	var IBPrototype = {
+		inst:IBProcesser(function(d,f){
+			return f.call(this,d,this.$select);
+		}),
+		build:function(){
+			var Inteface = this, f = function(c){
+				for(var i=0,q=Inteface.$que,l=q.length;i<l;i++) c = q[i].call(Inteface,c);
+				return c;
+			}.bind();
+			
+			f.bind = function(bindSelect){
+				return (new InterfaceDefine(bindSelect,Inteface.$que)).build();
+			};
+			
+			return f;
+		}
+	};
+	
+	IB.METHOD = function(k,v){
+		var props = CFAS.PROPS(k,v);
+		for(var key in props) ib[key] = props[key];
+		return props;
+	};
+	
+	IB.PROCESS = function(k,v){
+		var props = CFAS.PROPS(k,v);
+		for(var key in props) if(typeof props[key] === "function") IBPrototype[key] = IBProcesser(props[key]);
+		return props;
+	};
+	
+	IB.DUAL = function(k,v){
+		return IB.PROCESS(IB.METHOD(k,v));
+	};
+	
+	//default
+	IB.METHOD({
+		"break":CFBREAKER,
+		"props":CFAS.PROPS
+	});
+	
+	IB.DUAL({
+		"each":function(d,f){
+			for(var i=0,d=CFAS.DATA(d),l=d.length;i<l;i++) if( f(d[i],i) === CFBREAKER ) break;
+			return d;
+		},
+		"each":function(d,f){
+			for(var i=0,d=CFAS.DATA(d),l=d.length;i<l;i++) if( f(d[i],i) === CFBREAKER ) break;
+			return d;
+		},
+		"map":function(d,f){
+			var c;
+			for(var i=0,d=CFAS.DATA(d),l=d.length;i<l;i++) if( (c=f(d[i],i)) === CFBREAKER ) break; else (d[i]=c);
+			return d;
+		},
+		"inject":function(d,f,r){
+			var c,r=(typeof r === "object")?r:{};
+			for(var i=0,d=CFAS.DATA(d),l=d.length;i<l;i++) if( (c=f(r,d[i],i)) === CFBREAKER ) break;
+			return r;
+		},
+		"reduce":function(d,f,r){
+			var c;
+			for(var i=0,d=CFAS.DATA(d),l=d.length;i<l;i++) if( (c=f(d,d[i],i)) == CFBREAKER ) break; else r=c;
+			return r;
+		},
+		"forEach":function(d,f){
+			for(var k in d) if( f(d[k],k) === CFBREAKER ) return d;
+			return d;
+		},
+		"forMap":function(d,f){
+			var c;
+			for(var k in d) if( (c=f(d[k],k)) === CFBREAKER ) return d; else d[k] = c;
+			return d;
+		}
+	});
+	
+	IB.EXTEND = function(block){
+		if(typeof block === "function") block.call(IB,IB,CFIS,CFAS,TODO);
+	};
+	
+	var InterfaceDefine = window.InterfaceDefine = (function(){
+		var InterfaceDefine = function(select,que){
+			this.$select = select;
+			this.$que = CFAS.DATA(que);
+		};
+		
+		InterfaceDefine.prototype = IBPrototype;
+		
+		return InterfaceDefine;
+	}());
+	
+}(window));
---
+++ source/javascripts/ib.web.js
@@ -0,0 +1,412 @@
+ib && ib.EXTEND(function(IB,CFIS,CFAS,TODO){
+	
+	// addEventListener polyfill by https://gist.github.com/jonathantneal/3748027
+	!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+		WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+			var target = this;
+
+			registry.unshift([target, type, listener, function (event) {
+				event.currentTarget = target;
+				event.preventDefault = function () { event.returnValue = false };
+				event.stopPropagation = function () { event.cancelBubble = true };
+				event.target = event.srcElement || target;
+
+				listener.call(target, event);
+			}]);
+
+			this.attachEvent("on" + type, registry[0][3]);
+		};
+
+		WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
+			for (var index = 0, register; register = registry[index]; ++index) {
+				if (register[0] == this && register[1] == type && register[2] == listener) {
+					return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+				}
+			}
+		};
+
+		WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+			return this.fireEvent("on" + eventObject.type, eventObject);
+		};
+	})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
+	
+	IB.ENV = (function(){
+		var info = {};
+		
+		info.online = navigator ? navigator.onLine : false;
+		
+		//support LocalStorage
+		info.supportLocalStorage = window ? ('localStorage' in window) ? true : false : false;
+		info.localStorage = window.localStorage;
+		
+		//support SessionStorage
+		info.supportSessionStorage = window ? ('sessionStorage' in window) ? true : false : false;
+		info.sessionStorage = window.sessionStorage;
+		
+		//storage hack
+		info.supportStorage = info.supportLocalStorage || info.supportSessionStorage;
+		info.storage = info.localStorage || info.sessionStorage;
+		
+		//support ComputedStyle
+		info.supportComputedStyle  =  window ? ('getComputedStyle' in window) ? true : false : false;
+	
+		var lab3Prefix = function(s){
+			if( s.match(/^Webkit/) ) return "-webkit-";
+			if( s.match(/^Moz/) )    return "-moz-";
+			if( s.match(/^O/) )      return "-o-";
+			if( s.match(/^ms/) )     return "-ms-";
+			return "";
+		};
+	
+		var supportPrefix = {};
+	
+		info.getCSSName = function(cssName){
+			if(typeof cssName !== "string"){
+				return cssName+"";
+			}
+			cssName.trim();
+			for(var prefix in supportPrefix) {
+				if( cssName.indexOf(prefix) === 0 ) {
+					var sp = supportPrefix[prefix];
+					if( sp.length ) return sp+cssName;
+				}
+			}
+			return cssName;
+		};
+	
+		var tester = document.createElement('div');
+	
+		//transform
+		support = false;
+		"transform WebkitTransform MozTransform OTransform msTransform".replace(/\S+/g,function(s){ if(s in tester.style){
+			support = true;	
+			supportPrefix["transform"] = lab3Prefix(s);
+		}});
+		info.supportTransform = support;
+	
+		//transition
+		support = false;
+		"transition WebkitTransition MozTransition OTransition msTransition".replace(/\S+/g,function(s){ if(s in tester.style){
+			support = true;
+			supportPrefix["transition"] = lab3Prefix(s);
+		}});
+		info.supportTransition = support;
+	
+		//getUserMedia
+		info.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+		info.supportGetUserMedia = !!info.getUserMedia;
+	
+		//ie8 fix nodelist slice
+		info.supportNodeListSlice = (function(){try{Array.prototype.slice.call(NodeList);return true;}catch(e){return false;}}());
+	
+		//matches
+		info.querySelectorAllName =
+			('querySelectorAll'       in document) ? 'querySelectorAll' :
+			('webkitQuerySelectorAll' in document) ? 'webkitQuerySelectorAll' :
+			('msQuerySelectorAll'     in document) ? 'msQuerySelectorAll' :
+			('mozQuerySelectorAll'    in document) ? 'mozQuerySelectorAll' :
+			('oQuerySelectorAll'      in document) ? 'oQuerySelectorAll' : false;
+		info.supportQuerySelectorAll = !!info.querySelectorAllName;
+		info.supportStandardQuerySelectorAll = (info.querySelectorAllName === 'querySelectorAll');
+		
+		//matches
+		info.matchesSelectorName = 
+			('matches' in tester)               ? 'matches' :
+			('webkitMatchesSelector' in tester) ? 'webkitMatchesSelector' :
+			('msMatchesSelector'     in tester) ? 'msMatchesSelector' :
+			('mozMatchesSelector'    in tester) ? 'mozMatchesSelector' :
+			('oMatchesSelector'      in tester) ? 'oMatchesSelector' : false;
+		
+		info.supportMatches = !!info.matchesSelectorName;
+		info.supportStandardMatches = (info.matchesSelectorName === 'matches');
+		
+		return info;
+	}());
+	
+	var QUERY_SELECTOR_NAME = IB.ENV.querySelectorAllName;
+	IB.QUERY_SELECTOR_ENGINE = IB.ENV.supportQuerySelectorAll && IB.ENV.supportNodeListSlice ? 
+	function(node,selector){
+		try {
+			return Array.prototype.slice.call(
+				(node||document)[QUERY_SELECTOR_NAME](
+					selector.replace(/\[[\w\-\_]+\=[^\'\"][^\]]+\]/g, function(s){ 
+						return s.replace(/\=.+\]$/,function(s){ 
+							return '=\"' + s.substr(1,s.length-2) + '\"]'; 
+						}) 
+					})
+				)
+			);
+		} catch(e) {
+			console.error("IB::QUERY_SELECTOR_ENGINE error",node,selector);
+			if(IB.DEBUGER === true)debugger;
+		}
+	
+	}:
+	function(node,selector){
+		try {
+			var nodeList = (node||document)[QUERY_SELECTOR_NAME](
+				selector.replace(/\[[\w\-\_]+\=[^\'\"][^\]]+\]/g, function(s){ 
+					return s.replace(/\=.+\]$/,function(s){ 
+						return '=\"' + s.substr(1,s.length-2) + '\"]'; 
+					}) 
+				})
+			);
+		} catch(e) {
+			console.error("IB::QUERY_SELECTOR_ENGINE error",node,selector);
+			if(IB.DEBUGER === true)debugger;
+		}
+		var result = [];
+		for(var i=0,l=nodeList.length;i<l;i++){
+			nodeList[i] && result.push(nodeList[i]);
+		} 
+		return result;
+	};
+	//if natvie query selector in browser then alternative engine include
+	if(!IB.QUERY_SELECTOR_ENGINE){
+		if(typeof Sizzle === "function"){
+			console.info("nody is sizzle selector engine detected");
+			IB.QUERY_SELECTOR_ENGINE = function(node,selector){
+				return Sizzle(selector,node);
+			}
+			IB.QUERY_SELECTOR_ENGINE_ID = "sizzle";
+		} else if(typeof jQuery === "function") {
+			console.info("nody is jquery selector engine detected");
+			IB.QUERY_SELECTOR_ENGINE = function(node,selector){
+				return jQuery(selector,node).toArray();
+			}
+			IB.QUERY_SELECTOR_ENGINE_ID = "jquery";
+		}
+	} else {
+		IB.QUERY_SELECTOR_ENGINE_ID = "browser";
+	}
+	if(!IB.QUERY_SELECTOR_ENGINE){
+		IB.QUERY_SELECTOR_ENGINE_ID = null;
+		throw new Error("IB::ENV::IMPORTANT!! - querySelectorEngine is not detected");
+	}
+	var MATCHES_SELECTOR_NAME = IB.ENV.matchesSelectorName;
+	IB.MATCHES_SELECTOR_ENGINE = IB.ENV.supportMatches && function(node,selector){ 
+		//selectText fix
+		return node[MATCHES_SELECTOR_NAME](
+			selector.replace(/\[[\w\-\_]+\=[^\'\"][^\]]+\]/g, function(s){ 
+				return s.replace(/\=.+\]$/,function(s){ 
+					return '=\"' + s.substr(1,s.length-2) + '\"]'; 
+				}) 
+			})
+		); 
+	}; 
+	//if natvie matches selector in browser then alternative engine include
+	if(!IB.MATCHES_SELECTOR_ENGINE){
+		if(typeof Sizzle === "function"){
+			IB.MATCHES_SELECTOR_ENGINE = function(node,selector){
+				return Sizzle.matchesSelector(node,selector);
+			}
+			IB.MATCHES_SELECTOR_ENGINE_ID = "sizzle";
+		} else if(typeof jQuery === "function") {
+			IB.MATCHES_SELECTOR_ENGINE = function(node,selector){
+				return jQuery(node).is(selector);
+			}
+			IB.MATCHES_SELECTOR_ENGINE_ID = "jquery";
+		}
+	} else {
+		IB.MATCHES_SELECTOR_ENGINE_ID = "browser";
+	}
+	if(!IB.MATCHES_SELECTOR_ENGINE){
+		IB.MATCHES_SELECTOR_ENGINE_ID = null;
+		throw new Error("IB::ENV::IMPORTANT!! - matchesSelectorEngine is not detected");
+	}
+	
+	IB.ACTUAL_QUERY_SELECTOR = function(query,root){
+		//querySelectorSupport
+		if(typeof query !== "string" || (query.trim().length == 0)) return [];
+		root = ((typeof root === "undefined")?document:CFIS.ELEMENT(root)?root:document);
+		if(root == document) {
+			return IB.QUERY_SELECTOR_ENGINE(root,query);
+		} else {
+			if(IB.MATCHES_SELECTOR_ENGINE(root,query))
+			return [root].concat(Array.prototype.slice.call(IB.QUERY_SELECTOR_ENGINE(root,query)));
+			return IB.QUERY_SELECTOR_ENGINE(root,query);
+		}
+	};
+	
+	
+	// TODO :self replace and CFAS
+	TODO.UNIQUE = function(){
+		var value  = [],result = [];
+		for(var ai=0,li=arguments.length;ai<li;ai++){
+			var mvArray = CORE.NEW_ARRAY(arguments[ai]);
+			for(var i=0,l=mvArray.length;i<l;i++){
+				var unique = true;
+				for(var i2=0,l2=result.length;i2<l2;i2++){
+					if(mvArray[i] == result[i2]){
+						unique = false;
+						break;
+					}
+				}
+				if(unique==true) result.push(mvArray[i]);
+			}
+		}
+		return result;
+	};
+	
+	IB.METHOD({
+		"query":IB.ACTUAL_QUERY_SELECTOR,
+		"findLite":function(find){
+			if( typeof find === 'string' ){
+				// [string,null]
+				return IB.ACTUAL_QUERY_SELECTOR(find);
+			} else if(CFIS.ELEMENT(find)){
+				// [node]
+				return [find];
+			}  else if(CFIS.ARRAY(find)) {
+				// [array]
+				var fc = [];
+				for(var i=0,l=find.length;i<l;i++) { 
+					if( typeof find[i] === 'string' ) {
+						// [array][string]
+						var fs = IB.ACTUAL_QUERY_SELECTOR(find[i]);
+						if(fs.length) fc = fc.concat( fs );
+					} else if(CFIS.ELEMENT(find[i])) {
+						// [array][node]
+						fc.push(find[i]);
+					} else if(CFIS.ARRAY(find[i])){
+						var fa = IB.findLite(find[i]);
+						if(fa.length) fc = fc.concat( fa );
+					}
+				}
+				return TODO.UNIQUE(fc);
+			}
+			return [];
+		},
+		"attr":function(node,v1,v2){
+			if(!CFIS.ELEMENT(node)) { console.error("IB.attr은 element만 가능합니다. => 들어온값" + N.tos(node)); return null; }
+			if(arguments.length === 1) {
+				return IB.inject(node.attributes,function(inj,attr){
+					inj[attr.name] = node.getAttribute(attr.name);
+				});
+			} else if(typeof v1 === "object") {
+				for(var k in v1) node.setAttribute(k,v1[k]);
+			} else if(typeof v1 === "string"){
+				var readMode   = typeof v2 === "undefined";
+				var lowerKey = v1.toLowerCase();
+				switch(lowerKey){
+					case "readonly":
+						if("readOnly" in node){
+							if(readMode){
+								return node.readOnly;
+							} else {
+								node.readOnly = v2;
+								return node;
+							}
+						} 
+						break;
+					case "disabled": case "checked":
+						if(lowerKey in node){
+							if(readMode){
+								return node[lowerKey];
+							} else {
+								node[lowerKey] = v2;
+								return node;
+							}
+						}
+						break;
+				}
+				if(readMode){
+					var result = (node.getAttribute && node.getAttribute(v1)) || null;
+			        if( !result ) {
+			            var attrs = node.attributes;
+			            if(!CFIS.USELESS(attrs)){
+			            	var length = attrs.length;
+			            	for(var i = 0; i < length; i++)
+								if(attrs[i].nodeName === v1) {
+									result = attrs[i].value;
+									if(typeof result === "undefined") result = attrs[i].nodeValue;
+								} 
+			            }
+			        }
+			        return result;
+				} else {
+					node.setAttribute(v1,v2);
+				}
+			}
+			return node;
+		},
+		//get css style tag info
+		"trace":function(target,detail){
+			var t = IB.findLite(target)[0];
+			if( t ){
+				var tag = t.tagName.toLowerCase();
+				var tid = tclass = tname = tattr = tvalue = '';
+				IB.forEach(IB.attr(t),function(value,sign){
+					switch(sign){
+						case "id"   : 
+							var id = t.getAttribute(sign); 
+							id.length && (tid='#'+id) ; 
+							break;
+						case "class": 
+							tclass = t.getAttribute(sign).trim().replace(/\s\s/g,' ').split(' ').join('.'); 
+							if(tclass) tclass = "." + tclass;
+							break;
+						case "name" : tname  = "[name="+t.getAttribute(sign)+"]"; break;
+						case "value": break;
+						default     :
+							if(detail == true) {
+								attrValue = t.getAttribute(sign);
+								tattr += ( (attrValue == '' || attrValue == null) ? ("["+sign+"]") : ("["+sign+"="+attrValue+"]") );
+							}
+						break; 
+					}
+				});
+				if(detail == true) {
+					if(!/table|tbody|thead|tfoot|ul|ol/.test(tag)) {
+						var tv = N.node.value(t);
+						if(typeof tv !== undefined || tv !== null ) if(typeof tv === 'string' && tv.length !== 0) tvalue = '::'+tv;
+						if(typeof tvalue === 'string') tvalue = tvalue.trim();
+					}
+				}
+				return tag+tid+tclass+tname+tattr+tvalue;
+			} else {
+				console.warn("IB.trace::target is not element or selector // target =>",target);
+			}
+		},
+		"findByOnePlace":function(findse,rootNode){
+			if(typeof findse === 'string') return IB.ACTUAL_QUERY_SELECTOR(findse,rootNode);
+			if( CFIS.ELEMENT(findse) ) {
+				var fs = IB.ACTUAL_QUERY_SELECTOR(IB.trace(findse),rootNode);
+				for(var i=0,l=fs.length;i<l;i++) if(findse === fs[i]) return [findse];
+			}
+			if( CFIS.ARRAY(findse) ) {
+				var result = [];
+				for(var i=0,l=findse.length;i<l;i++) {
+					var fd = IB.findByOnePlace(findse[i],rootNode);
+					if( fd.length ) result = result.concat(fd);
+				}				
+				return TODO.UNIQUE(result);
+			}
+			return [];
+		},
+		"findBySeveralPlaces":function(find,root){
+			if(arguments.length === 1 || typeof root === 'undefined' || root === null || root === window.document ) return IB.findLite(find);
+			// find root
+			var targetRoots = IB.findLite(root);
+			if(targetRoots.length === 0) {
+				return IB.findLite(find);
+			}
+			//
+			var findes = CFAS.ARRAY(find);
+			var result = [];
+			for(var i=0,l=targetRoots.length;i<l;i++) {
+				for(var fi=0,fl=findes.length;fi<fl;fi++) {
+					var fdr = IB.findByOnePlace(findes[fi],targetRoots[i]);
+					if( fdr.length ) result = result.concat(fdr);
+				}
+			}
+			return TODO.UNIQUE(result);
+		},
+		//최적화 분기하여 샐랙터를 실행시킴
+		"find" : function(find,root,eq){
+			return (typeof root === "number") ? IB.findLite(find)[root] :
+			(typeof eq === "number") ? IB.findBySeveralPlaces(find,root)[eq] : IB.findBySeveralPlaces(find,root);
+		}
+	});
+	
+});
