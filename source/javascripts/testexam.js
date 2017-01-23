63e1532f62c2acb2691ac1944999ca07efacd64c
HO-JUNG AHN <labeldock@me.com>
2017-01-24 01:04:07 +0900

    update sample

--- source/javascripts/testexam.js
+++ source/javascripts/testexam.js
@@ -1,12 +1,44 @@
 (function(){
-	var data = [
-		{data:12},
-		{data:24},
-		{data:44}
-	];
+	
 	
 	ib.directive("[role-space]",function(element,dataset,attr){
-		console.log("role space",element,dataset,attr);
-	})
+		var $element = $(element);
+		
+		var data = [
+			{value:0,size:10},
+			{value:24,size:10},
+			{value:40,size:10}
+		];
+		
+		var space = ib.space([0,50],[0,$element.width()]);
+		
+		ib.each(data,function(datum){
+			var $li = $("<li/>");
+			$element.append($li);
+		});
+		
+		var update = function(){
+			$element.children().each(function(index){
+				var datum = data[index];
+				var $li = $(this);
+				$li.text(datum.value + " : " + datum.size);
+				space.block(datum.value,datum.size).call(function(){
+					$li.css({
+						left:this.rangeStart(),
+						width:this.rangeSize() + "px"
+					});
+				});
+			});
+		};
+		
+		setInterval(function(){
+			ib.each(data,function(datum){	
+				datum.value += 1*(ib.versus()?-1:1);
+				datum.size += 1*(ib.versus()?-1:1);
+			});
+			
+			update();
+		},250);
+	});
 	
 }());
